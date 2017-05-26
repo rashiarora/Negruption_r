c10e1fdae1e7dcd28ebd90adfaf9539437133d6d
reaya <reaya.github@gmail.com>
2017-05-26 12:25:23 +0530

    Eigth Commit

--- app/src/main/java/com/codeogic/negruption/HomeActivity.java
+++ app/src/main/java/com/codeogic/negruption/HomeActivity.java
@@ -1,12 +1,16 @@
+
 package com.codeogic.negruption;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -17,11 +21,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -35,17 +43,20 @@ import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener,View.OnClickListener,AdapterView.OnItemClickListener, SwipeRefreshLayout.OnRefreshListener{
+        implements NavigationView.OnNavigationItemSelectedListener,AdapterView.OnItemClickListener, SwipeRefreshLayout.OnRefreshListener{
 
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
     TextView title;
+    EditText eTxtSearch;
     ListView listStories;
     ArrayList<StoryBean> stories;
     StoryAdapter adapter;
-    StoryBean storyBean;
+    StoryBean story;
     RequestQueue requestQueue;
     //ProgressDialog progressDialog;
     SwipeRefreshLayout swipeRefreshLayout;
@@ -60,11 +71,57 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       // title=(TextView)findViewById(R.id.textViewTitle);
+        // title=(TextView)findViewById(R.id.textViewTitle);
+        eTxtSearch = (EditText)findViewById(R.id.editTextSearch);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
         listStories = (ListView)findViewById(R.id.listStories);
-
         swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefresh);
+        listStories.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
 
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                boolean enable = false;
+
+                if(listStories != null && listStories.getChildCount() > 0){
+                    // check if the first item of the list is visible
+                    boolean firstItemVisible = listStories.getFirstVisiblePosition() == 0;
+                    // check if the top of the first item is visible
+                    boolean topOfFirstItemVisible = listStories.getChildAt(0).getTop() == 0;
+                    // enabling or disabling the refresh layout
+                    enable = firstItemVisible && topOfFirstItemVisible;
+                }
+                swipeRefreshLayout.setEnabled(enable);
+
+
+            }
+        });
+
+
+
+        eTxtSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String str = charSequence.toString();
+                if(adapter!=null){
+                    adapter.filter(str);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -78,7 +135,7 @@ public class HomeActivity extends AppCompatActivity
 
 
         sharedPreferences=getSharedPreferences(Util.PREFS_NAME,MODE_PRIVATE);
-       editor= sharedPreferences.edit();
+        editor= sharedPreferences.edit();
 
         String username=sharedPreferences.getString(Util.PREFS_KEYUSERNAME,"");
         //title.setText("Welcome Home "+username);
@@ -87,7 +144,7 @@ public class HomeActivity extends AppCompatActivity
         progressDialog.setCancelable(false);
 */
         requestQueue = Volley.newRequestQueue(this);
-       // retrieveStory();
+        // retrieveStory();
 
         swipeRefreshLayout.setOnRefreshListener(this);
 
@@ -206,7 +263,7 @@ public class HomeActivity extends AppCompatActivity
 
                     listStories.setAdapter(adapter);
                     listStories.setOnItemClickListener(HomeActivity.this);
-                   // progressDialog.dismiss();
+                    // progressDialog.dismiss();
 
                     swipeRefreshLayout.setRefreshing(false);
 
@@ -224,7 +281,7 @@ public class HomeActivity extends AppCompatActivity
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-               // progressDialog.dismiss();
+                // progressDialog.dismiss();
                 Toast.makeText(HomeActivity.this,"Some Error"+error,Toast.LENGTH_LONG).show();
                 swipeRefreshLayout.setRefreshing(false);
 
@@ -239,23 +296,95 @@ public class HomeActivity extends AppCompatActivity
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        storyBean = stories.get(position);
-        Toast.makeText(HomeActivity.this,"You Clicked"+storyBean.getUsername(),Toast.LENGTH_LONG).show();
+        story = stories.get(position);
+
+        story.setViews(story.getViews()+1);
+        Intent intent = new Intent(HomeActivity.this,StoryActivity.class);
+        intent.putExtra("keyStory",story);
+        startActivity(intent);
+
+        Task t = new Task();
+        t.execute();
+        Toast.makeText(HomeActivity.this,"You Clicked"+story.getUsername(),Toast.LENGTH_LONG).show();
         Log.i("HomeActivity","homeActivity");
 
 
     }
 
 
-    @Override
-    public void onClick(View v) {
 
+    class  Task extends AsyncTask {
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            StringRequest request = new StringRequest(Request.Method.POST, Util.UPDATE_VIEWS, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try{
+                        JSONObject jsonObject = new JSONObject(response);
+                        int success = jsonObject.getInt("success");
+                        String message = jsonObject.getString("message");
+
+                        if(success == 1){
+
+                            //Toast.makeText(ManageAccountActivity.this,message,Toast.LENGTH_LONG).show();
+                            //Intent i = new Intent(ManageAccountActivity.this,SplashActivity.class);
+                            //startActivity(i);
+                            // finish();
+                            Log.i("success",message);
+                        }else{
+                            // Toast.makeText(ManageAccountActivity.this,message,Toast.LENGTH_LONG).show();
+                        }
+                        // progressDialog.dismiss();
+                    }catch (Exception e){
+                        e.printStackTrace();
+
+                        Log.i("exception",e.getMessage());
+                        //progressDialog.dismiss();
+                        //Toast.makeText(ManageAccountActivity.this,"Some Exception"+e,Toast.LENGTH_LONG).show();
+                    }
+
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.i("error",error.getMessage());
+
+                }
+            })
+            {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+
+                    Map<String,String> map = new HashMap<>();
+                    map.put("views",String.valueOf(story.getViews()));
+                    map.put("sid",String.valueOf(story.getStoryId()));
+
+                    return map;
+                }
+            };
+            requestQueue.add(request);request.setRetryPolicy(new DefaultRetryPolicy(50000,
+                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+            requestQueue.add(request);
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            //adapter.notifyDataSetChanged();
+        }
     }
+
+
 
     @Override
     public void onRefresh() {
+        eTxtSearch.setText("");
         retrieveStory();
 
     }
 }
-
